d359e851b915e03e7c87ed742ef73a29593c9b5c
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-05-24 00:27:21 -0300

    edit cambios

--- controllers/publicaciones/update.js
+++ controllers/publicaciones/update.js
@@ -3,26 +3,51 @@ import Publicacion from "../../models/Publicacion.js";
 const updateP = {
     update: async (req, res, next) => {
         try {
-            let publicacion = await Publicacion.findOneAndUpdate(
-                { _id: req.params.id },
-                req.body,
-                { new: true }
-            );
-            if (publicacion) {
-                return res.status(200).json({
-                    success: true,
-                    publicacion
-                });
-            } else {
+            const { id } = req.params;
+
+            // Verificar si la publicación existe
+            let publicacion = await Publicacion.findById(id);
+            if (!publicacion) {
                 return res.status(404).json({
                     success: false,
-                    message: "Publicacion not found"
+                    message: "Publicacion no encontrada",
                 });
             }
+
+            // Actualizar los datos de la publicación si se proporcionan
+            const { title, description, categoria, cover_photo } = req.body;
+
+            const updatedFields = {};
+
+            if (title) {
+                updatedFields.title = title;
+            }
+            if (description) {
+                updatedFields.description = description;
+            }
+            if (categoria) {
+                updatedFields.categoria = categoria;
+            }
+            if (cover_photo) {
+                updatedFields.cover_photo = cover_photo;
+            }
+
+            // Actualizar la publicación
+            publicacion = await Publicacion.findByIdAndUpdate(
+                id,
+                { $set: updatedFields },
+                { new: true }
+            );
+
+            return res.status(200).json({
+                success: true,
+                message: "Publicacion actualizada exitosamente",
+                publicacion: publicacion,
+            });
         } catch (error) {
             next(error);
         }
-    }
+    },
 };
 
 export default updateP;
--- controllers/trabajos/update.js
+++ controllers/trabajos/update.js
@@ -3,22 +3,47 @@ import Trabajo from "../../models/Trabajos.js";
 const updateP = {
     update: async (req, res, next) => {
         try {
-            let trabajo = await Trabajo.findOneAndUpdate(
-                { _id: req.params.id },
-                req.body,
-                { new: true }
-            );
-            if (trabajo) {
-                return res.status(200).json({
-                    success: true,
-                    trabajo
-                });
-            } else {
+            let trabajo = await Trabajo.findOne({ _id: req.params.id });
+
+            if (!trabajo) {
                 return res.status(404).json({
                     success: false,
                     message: "trabajo not found"
                 });
             }
+
+            const updatedFields = {
+                jornada: req.body.jornada || trabajo.jornada,
+                requisitos: req.body.requisitos || trabajo.requisitos,
+                title: req.body.title || trabajo.title,
+                cover_photo: req.body.cover_photo || trabajo.cover_photo,
+                description: req.body.description || trabajo.description,
+                categoria: req.body.categoria || trabajo.categoria,
+                ubicacion: req.body.ubicacion || trabajo.ubicacion,
+                lugar: req.body.lugar || trabajo.lugar,
+                salario: req.body.salario || trabajo.salario,
+                vacantes: req.body.vacantes || trabajo.vacantes,
+                seller_id: req.body.seller_id || trabajo.seller_id,
+                category_id: req.body.category_id || trabajo.category_id,
+                name: req.body.name || trabajo.name,
+                photo: req.body.photo || trabajo.photo,
+                mail: req.body.mail || trabajo.mail,
+                telefono: req.body.telefono || trabajo.telefono,
+                web: req.body.web || trabajo.web,
+                instagram: req.body.instagram || trabajo.instagram,
+                facebook: req.body.facebook || trabajo.facebook
+            };
+
+            trabajo = await Trabajo.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: updatedFields },
+                { new: true }
+            );
+
+            return res.status(200).json({
+                success: true,
+                trabajo
+            });
         } catch (error) {
             next(error);
         }
